a424e9c0b964113e3d930b05f9511ecedb95e15a
soobig666 <darkvader.ukr.net@gmail.com>
2023-11-04 23:21:18 +0200

    Add env varibale INTERNAL_IP && Add CSRF_SUBDOMAIN

--- core/settings.py
+++ core/settings.py
@@ -36,16 +36,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "0.0.0.0",
-]
-INTERNAL_IPS = [
-    "127.0.0.1",
-    "localhost",
-    "0.0.0.0",
-]
+ALLOWED_HOSTS = [os.getenv("INTERNAL_IP")]
+INTERNAL_IPS = [os.getenv("INTERNAL_IP")]
 
 
 # Application definition
